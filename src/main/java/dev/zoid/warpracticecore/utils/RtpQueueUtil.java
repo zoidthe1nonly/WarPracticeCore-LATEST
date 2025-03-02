8615acd58b525c6fb16a7c6b8b69aa488a71db74
zoidthe1nonly <fockbeingirrational@gmail.com>
2025-03-02 06:46:02 -0800

    fix 2nd player sending queue message even though he found a bond, improved readablity

--- src/main/java/dev/zoid/warpracticecore/utils/RtpQueueUtil.java
+++ src/main/java/dev/zoid/warpracticecore/utils/RtpQueueUtil.java
@@ -21,81 +21,97 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class RtpQueueUtil implements Listener {
 
-    private static final MiniMessage miniMessage = MiniMessage.miniMessage();
-    private static final Set<Player> queue = new HashSet<>();
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+    private static final Set<Player> QUEUE = ConcurrentHashMap.newKeySet();
     private static final Map<String, List<Location>> CACHED_LOCATIONS = new ConcurrentHashMap<>();
     private static final int INITIAL_CACHE_SIZE = 5;
-    private static final Random random = new Random();
+    private static final Random RANDOM = new Random();
+    private static final int SLOW_FALLING_DURATION = 60;
+    private static final int SLOW_FALLING_AMPLIFIER = 1;
 
     public static void addToQueue(Player player) {
-        if (queue.contains(player)) {
+        if (QUEUE.contains(player)) {
             leaveQueue(player);
             return;
         }
-        queue.add(player);
-        sendActionBar(player, miniMessage.deserialize("<#e3b3bd>You're now in rtp queue"));
-        broadcastMessage(miniMessage.deserialize("<#1e90ff>" + player.getName() + " <#e3b3bd>is looking for a duel (/rtpqueue)"));
-        if (queue.size() >= 2) {
-            Iterator<Player> it = queue.iterator();
-            Player p1 = it.next();
-            Player p2 = it.next();
-            queue.remove(p1);
-            queue.remove(p2);
-            findRandomLocation("desert").thenAccept(location -> {
-                if (location != null) {
-                    Location loc1 = location.clone().add(10, 10, 0);
-                    Location loc2 = location.clone().add(-10, 10, 0);
-                    loc1.setDirection(new Vector(-1, 0, 0));
-                    loc2.setDirection(new Vector(1, 0, 0));
-                    p1.teleportAsync(loc1);
-                    p2.teleportAsync(loc2);
-                    applySlowFalling(p1);
-                    applySlowFalling(p2);
-                    Component m1 = miniMessage.deserialize("<#e3b3bd>You're now fighting <#1e90ff>" + p2.getName());
-                    Component m2 = miniMessage.deserialize("<#e3b3bd>You're now fighting <#1e90ff>" + p1.getName());
-                    sendActionBar(p1, m1);
-                    sendActionBar(p2, m2);
-                    sendMessage(p1, m1);
-                    sendMessage(p2, m2);
-                }
-            });
+
+        QUEUE.add(player);
+        sendActionBar(player, MINI_MESSAGE.deserialize("<#e3b3bd>You're now in rtp queue"));
+
+        if (QUEUE.size() >= 2) {
+            matchPlayers();
         } else {
-            sendActionBar(player, miniMessage.deserialize("<#e3b3bd>Searching for an opponent..."));
+            broadcastMessage(MINI_MESSAGE.deserialize("<#1e90ff>" + player.getName() + " <#e3b3bd>is looking for a duel (/rtpqueue)"));
+            sendActionBar(player, MINI_MESSAGE.deserialize("<#e3b3bd>Searching for an opponent..."));
         }
     }
 
+    private static void matchPlayers() {
+        Iterator<Player> iterator = QUEUE.iterator();
+        Player player1 = iterator.next();
+        Player player2 = iterator.next();
+        QUEUE.remove(player1);
+        QUEUE.remove(player2);
+
+        findRandomLocation("desert").thenAccept(location -> {
+            if (location != null) {
+                teleportPlayers(player1, player2, location);
+            }
+        });
+    }
+
+    private static void teleportPlayers(Player player1, Player player2, Location location) {
+        Location loc1 = location.clone().add(10, 10, 0);
+        Location loc2 = location.clone().add(-10, 10, 0);
+        loc1.setDirection(new Vector(-1, 0, 0));
+        loc2.setDirection(new Vector(1, 0, 0));
+
+        player1.teleportAsync(loc1);
+        player2.teleportAsync(loc2);
+        applySlowFalling(player1);
+        applySlowFalling(player2);
+
+        Component message1 = MINI_MESSAGE.deserialize("<#e3b3bd>You're now fighting <#1e90ff>" + player2.getName());
+        Component message2 = MINI_MESSAGE.deserialize("<#e3b3bd>You're now fighting <#1e90ff>" + player1.getName());
+
+        sendActionBar(player1, message1);
+        sendActionBar(player2, message2);
+        sendMessage(player1, message1);
+        sendMessage(player2, message2);
+    }
+
     public static void leaveQueue(Player player) {
-        if (queue.remove(player)) {
-            Component m = miniMessage.deserialize("<red>You have left the queue");
-            sendActionBar(player, m);
-            sendMessage(player, m);
+        if (QUEUE.remove(player)) {
+            Component message = MINI_MESSAGE.deserialize("<red>You have left the queue");
+            sendActionBar(player, message);
+            sendMessage(player, message);
         }
     }
 
     public static void preloadRandomLocations() {
-        for (World world : Bukkit.getWorlds()) {
+        Bukkit.getWorlds().forEach(world -> {
             String worldName = world.getName();
             CACHED_LOCATIONS.putIfAbsent(worldName, new CopyOnWriteArrayList<>());
             for (int i = 0; i < INITIAL_CACHE_SIZE; i++) {
                 generateRandomLocationAsync(world).thenAccept(loc -> CACHED_LOCATIONS.get(worldName).add(loc));
             }
-        }
+        });
     }
 
     private static CompletableFuture<Location> findRandomLocation(String worldName) {
         List<Location> cache = CACHED_LOCATIONS.get(worldName);
         if (cache != null && !cache.isEmpty()) {
-            Location loc = cache.remove(0);
+            Location location = cache.remove(0);
             Bukkit.getScheduler().runTaskAsynchronously(WarPracticeCore.plugin(), () -> {
-                World w = Bukkit.getWorld(worldName);
-                if (w != null) {
-                    generateRandomLocationAsync(w).thenAccept(newLoc -> CACHED_LOCATIONS.get(worldName).add(newLoc));
+                World world = Bukkit.getWorld(worldName);
+                if (world != null) {
+                    generateRandomLocationAsync(world).thenAccept(newLocation -> CACHED_LOCATIONS.get(worldName).add(newLocation));
                 }
             });
-            return CompletableFuture.completedFuture(loc);
+            return CompletableFuture.completedFuture(location);
         }
-        World w = Bukkit.getWorld(worldName);
-        return w != null ? generateRandomLocationAsync(w) : CompletableFuture.completedFuture(null);
+        World world = Bukkit.getWorld(worldName);
+        return world != null ? generateRandomLocationAsync(world) : CompletableFuture.completedFuture(null);
     }
 
     private static CompletableFuture<Location> generateRandomLocationAsync(World world) {
@@ -104,10 +120,11 @@ public class RtpQueueUtil implements Listener {
         double size = border.getSize() / 2;
         double centerX = border.getCenter().getX();
         double centerZ = border.getCenter().getZ();
-        double x = centerX + (random.nextDouble() * size * 2 - size);
-        double z = centerZ + (random.nextDouble() * size * 2 - size);
+        double x = centerX + (RANDOM.nextDouble() * size * 2 - size);
+        double z = centerZ + (RANDOM.nextDouble() * size * 2 - size);
         int chunkX = ((int) x) >> 4;
         int chunkZ = ((int) z) >> 4;
+
         world.getChunkAtAsync(chunkX, chunkZ).thenAccept(chunk -> {
             int y = world.getHighestBlockYAt((int) x, (int) z);
             future.complete(new Location(world, x, y, z));
@@ -115,11 +132,12 @@ public class RtpQueueUtil implements Listener {
             future.completeExceptionally(ex);
             return null;
         });
+
         return future;
     }
 
     private static void applySlowFalling(Player player) {
-        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 60, 1));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, SLOW_FALLING_DURATION, SLOW_FALLING_AMPLIFIER));
     }
 
     private static void sendActionBar(Player player, Component message) {
